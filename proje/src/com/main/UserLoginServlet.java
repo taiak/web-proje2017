f5b0a5db392983fe189b61d3b7688d1b57cf0357
ileri <celil.ileri@bil.omu.edu.tr>
2018-05-10 04:52:29 +0300

    create user login servlet

---
+++ proje/src/com/main/UserLoginServlet.java
@@ -0,0 +1,27 @@
+package com.main;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/UserLoginServlet")
+public class UserLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public HttpServletRequest request;
+    public HttpServletResponse response;
+    
+	public UserLoginServlet(HttpServletRequest request, HttpServletResponse response) {
+	    this.request = request;
+	    this.response = response;
+    }
+
+	public void index() throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("userLogin.jsp");
+        dispatcher.forward(request, response);
+	}
+}
