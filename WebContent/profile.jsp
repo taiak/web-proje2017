5cf23bd31c0a6600094b35b5122d880e5a608744
ileri <celil.ileri@bil.omu.edu.tr>
2018-05-11 21:58:25 +0300

    show completed order in customer profile page

--- WebContent/profile.jsp
+++ WebContent/profile.jsp
@@ -1,10 +1,38 @@
 <%@ taglib prefix="z" tagdir="/WEB-INF/tags" %>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <z:layout title="Profil">
 	<div class="container">
 		<div class="row">
 			<div class="col-9">
 				<h1 class="display-1">${user.name} ${user.surname}</h1>
+				<h2>Tamamlanan Siparişlerim</h2>
+				<table class="table table-hover table-striped">
+		    		<thead>
+						<tr>
+							<th>Ürün Fotoğrafı</th>
+							<th>Ürün Adı</th>
+							<th>Fiyat</th>
+						</tr>
+				    </thead>
+				    <tbody>
+						<c:forEach var="order" items="${userOrders}" varStatus="loop">
+					      <tr>
+					      	<td>
+					      		<a href="product?id=${order.productNo}">
+					      			<img src="${products.get(loop.index).photo}" width="100" height="100">
+					      		</a>
+					      	</td>
+					        <td><strong>${products.get(loop.index).name}</strong></td>
+					        <td><strong>${products.get(loop.index).price} TL</strong></td>
+					      </tr>
+				      	</c:forEach>
+				    </tbody>
+				 </table>
+				<div class="row">
+			      	<div class="col-9"><h1>Tüm Harcamalar Toplamı:</h1></div>
+			      	<div class="col-3"><h1><strong>${allCompletedOrdersPriceSum} TL</strong></h1></div>
+		      	</div>
 			</div>
 			<div class="col-3">
 				<img src="https://www.iconspng.com/uploads/abstract-user-icon-3/abstract-user-icon-3.png" height="300px" width="300px"/>
--- src/com/proje/controller/main/ProfileController.java
+++ src/com/proje/controller/main/ProfileController.java
@@ -1,6 +1,8 @@
 package com.proje.controller.main;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.proje.DAO.OrderDAO;
+import com.proje.DAO.ProductDAO;
+import com.proje.model.Order;
+import com.proje.model.Product;
 import com.proje.model.User;
 
 @WebServlet("/ProfileServlet")
@@ -23,8 +29,26 @@ public class ProfileController extends HttpServlet {
 
 	public void index() throws ServletException, IOException {
 		if (com.proje.controller.login.LoginController.session != null) {
-			User user = (User) com.proje.controller.login.LoginController.session.getAttribute("user");
+			User user = (User) com.proje.controller.login.LoginController.session.getAttribute("user");			
+			ArrayList<Order> userOrders = OrderDAO.getOrderByUserId(Integer.parseInt(user.getId()));
+	        ArrayList <Product> products = new ArrayList<Product>();
+
+			Order order;
+			Product product;
+	        float allCompletedOrdersPriceSum = 0;
+	        
+	        for (int i = 0; i < userOrders.size(); i++) {
+				order = userOrders.get(i);
+				product = ProductDAO.find(order.getProductNo());
+				allCompletedOrdersPriceSum += Float.parseFloat(product.getPrice());
+				products.add(product);
+			}
+	        
 			request.setAttribute("user", user);
+			request.setAttribute("userOrders", userOrders);
+			request.setAttribute("products", products);
+			request.setAttribute("allCompletedOrdersPriceSum", allCompletedOrdersPriceSum);
+
 	        RequestDispatcher dispatcher = request.getRequestDispatcher("profile.jsp");
 	        dispatcher.forward(request, response);
 		}else {
